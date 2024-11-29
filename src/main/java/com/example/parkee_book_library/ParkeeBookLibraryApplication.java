6be5d3ce9f7adfd2cf0542d4fea5079c22eb31cc
Reno Fathoni <renofathoni23@gmail.com>
2024-11-29 13:10:37 +0700

    first commit

---
+++ src/main/java/com/example/parkee_book_library/ParkeeBookLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkee_book_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkeeBookLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkeeBookLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/parkee_book_library/ParkeeBookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkee_book_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkeeBookLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
